03904c7caf68955acde6baf60c3d7171285e25da
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2023-06-01 15:52:46 +0600

    [Done] husky setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import cors from "cors";
-const app = express();
+import express, { Application } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+    app.use(express.json())
+       app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function run_server() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("database connected successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('database connected successfully')
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
-run_server();
+run_server()
